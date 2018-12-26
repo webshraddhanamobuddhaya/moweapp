dea8d63023bb2fd17ae7d1e4a4a4486cd58a7a14
upathissa <amilaupathissa@gmail.com>
2018-12-26 18:51:55 +0530

    ok

--- resources/assets/js/components/Contact.vue
+++ resources/assets/js/components/Contact.vue
@@ -1,102 +1,82 @@
 <template>
-    <v-flex xs12 sm6 offset-sm3>
-        <v-card id="message-card">
-            <v-form ref="form" v-model="valid" lazy-validation>
-            <v-text-field
-                v-model="name"
-                :rules="nameRules"
-                :counter="20"
-                label="Your Name"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="subject"
-                :rules="subjectRules"
-                :counter="50"
-                label="Subject"
-                required
-            ></v-text-field>
+  <v-flex xs12 sm6 offset-sm3>
+    <v-card id="message-card">
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field v-model="name" :rules="nameRules" :counter="20" label="Your Name" required></v-text-field>
+        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+        <v-text-field
+          v-model="subject"
+          :rules="subjectRules"
+          :counter="50"
+          label="Subject"
+          required
+        ></v-text-field>
 
-            <v-textarea
-                v-model="message"
-                :rules="messageRules"
-                label="Your Message"
-                auto-grow
-                value=""
-                required
-            ></v-textarea>
+        <v-textarea
+          v-model="message"
+          :rules="messageRules"
+          label="Your Message"
+          auto-grow
+          value
+          required
+        ></v-textarea>
 
-
-            <v-btn
-                :disabled="!valid"
-                @click="submit"
-            >
-                submit
-            </v-btn>
-            <v-btn @click="clear">clear</v-btn>
-            </v-form>
-        </v-card>
-    </v-flex>
+        <v-btn :disabled="!valid" @click="submit">submit</v-btn>
+        <v-btn @click="clear">clear</v-btn>
+      </v-form>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import store from "../store/store";
-  export default {
-    data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters'
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ],
-      subject: '',
-      subjectRules: [
-        v => !!v || 'Subject is required',
-        v => (v && v.length <= 50) || 'Name must be less than 50 characters'
-      ],
-      message: '',
-      messageRules: [
-          v=> !!v || 'Message is required'
-      ]
-
-    }),
-    created(){
-        store.dispatch("setNavTitle",'Send Your Message');
-    },
-    methods: {
-      submit () {
-        if (this.$refs.form.validate()) {
-          // Native form submission is not yet supported
-          axios.post('/api/submit', {
-            name: this.name,
-            email: this.email,
-            select: this.select,
-            checkbox: this.checkbox
-          })
-        }
-      },
-      clear () {
-        this.$refs.form.reset()
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Name is required",
+      v => (v && v.length <= 10) || "Name must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    subject: "",
+    subjectRules: [
+      v => !!v || "Subject is required",
+      v => (v && v.length <= 50) || "Name must be less than 50 characters"
+    ],
+    message: "",
+    messageRules: [v => !!v || "Message is required"]
+  }),
+  created() {
+    store.dispatch("setNavTitle", "Send Your Message");
+  },
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) {
+        // Native form submission is not yet supported
+        axios.post("/api/submit", {
+          name: this.name,
+          email: this.email,
+          select: this.select,
+          checkbox: this.checkbox
+        });
       }
+    },
+    clear() {
+      this.$refs.form.reset();
     }
   }
+};
 </script>
 <style>
-#message-card{
-    padding:15px;
-    padding-bottom:50px;
-    margin-bottom: 200px;
+#message-card {
+  padding: 15px;
+  padding-bottom: 50px;
+  margin-bottom: 200px;
 }
 </style>
---
+++ resources/assets/js/components/Radio-bakup.vue
@@ -0,0 +1,184 @@
+<template>
+	<div id="radio">
+		<audio ref="radio" controls id="radio" v-show="false">
+			<source src='http://69.46.24.226:7643/;'>
+		</audio>
+		
+		<v-flex xs12 sm6 offset-sm3>
+			<v-card>
+				<div >
+						<img src="/images/lakviru.jpg" alt="Vuetify.js" id="lakviru-logo">
+				</div>
+
+						
+					<v-progress-circular
+						:rotate="360"
+						:size="175"
+						:width="25"
+						:value="volume_value"
+						:color="color"
+					>
+					<v-btn :color="color" fab large dark @click="mainControll">
+							<v-icon>{{button_icon}}</v-icon>
+						</v-btn>
+					</v-progress-circular>
+
+
+					<v-flex xs10 offset-xs1>
+						<v-slider
+							@change="volueChange"
+							v-model="volume_value"
+							:color="color"
+							always-dirty
+							min="0"
+							max="100"
+							>
+							<v-icon
+								slot="prepend"
+								:color="color"
+								@click="decrement"
+							>
+								exposure_neg_1
+							</v-icon>
+					
+							<v-icon
+								slot="append"
+								:color="color"
+								@click="increment"
+							>
+								plus_one
+							</v-icon>
+						</v-slider>
+					<P class="subheading" id="volumeValue">Volume: {{volume_value}}%</P>
+					</v-flex>
+
+
+			</v-card>
+		</v-flex>
+
+
+	</div>
+</template>
+
+<script>
+import store from "../store/store";
+export default {
+	data(){
+		return {
+			volume_value: 60,
+			button_icon: "play_arrow",
+			audio: undefined,
+			playing: false,
+			color: "blue-grey lighten-3",
+			inactive_color: "blue-grey lighten-3"
+		}
+	},
+	computed: {
+		volumeDisplay(){
+	
+			return Math.round(this.volume_value*100);
+		},
+		colors () {
+			if (this.volume_value < 30) return 'indigo'
+			if (this.volume_value < 50) return 'teal'
+			if (this.volume_value < 70) return 'green'
+			if (this.volume_value < 90) return 'orange'
+			return 'red'
+		}
+	},
+	watch:{
+		volume_value(value){
+			if(this.playing){
+				this.colorBaseValue(value)	
+			}else{
+				this.color = this.inactive_color
+			}
+
+		},
+		playing(value){
+
+		}
+	},
+	created(){
+		store.dispatch("setNavTitle",'Radio Live Stream');
+	},
+	mounted(){
+		this.$refs.radio.load();
+		let playPromise = this.$refs.radio.play();
+		if (playPromise !== undefined) {
+			playPromise.then(_ => {
+			// Automatic playback started!
+			// Show playing UI.
+			console.log('start playing')
+			})
+			.catch(error => {
+			// Auto-play was prevented
+			// Show paused UI.
+			console.log(error)
+			});
+		}
+		this.changePlayButton();
+	},
+
+	methods:{
+		colorBaseValue(value){
+			if(value>90){this.color = "red accent-1"}
+				else if(value>70){this.color = "teal"}
+				else if(value>50){this.color = "green"}
+				else if(value>30){this.color = "orange"}
+				else{this.color = "indigo lighten-3"}
+		},
+		volueChange(){
+			this.$refs.radio.volume = this.volume_value/100;
+		},
+		//Change play button to pause and do basic changes
+		changePlayButton(){
+			setTimeout(() => this.button_icon = "pause", 1000);
+			
+			this.playing = true;
+			this.colorBaseValue(this.volume_value);
+		},
+		clickForPlay(){
+			this.button_icon = "pause";
+			this.playing = true;
+			this.colorBaseValue(this.volume_value);
+
+		},
+		clickForPause(){
+			this.button_icon = "play_arrow";
+			this.playing = false;
+			this.color = this.inactive_color;
+
+		},
+		mainControll(){
+			var a = this.$refs.radio;
+			if (a.paused) {
+				a.play();
+				this.clickForPlay();
+	
+			} else {
+				a.pause();
+				this.clickForPause();
+			}
+		},
+		increment () {
+			this.volume_value++;
+		},
+		decrement () {
+			this.volume_value--;
+		}
+	}
+}
+</script>
+
+<style>
+	#lakviru-logo{
+		max-width: 60%;
+	}
+	#volumeValue{
+
+    margin-top: -30px;
+    padding-bottom: 25px;
+	color: #B0BEC5;
+	}
+</style>
